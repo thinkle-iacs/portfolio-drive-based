b63d376ac44b5367c1372ac96f921026ea6fec71
Tom Hinkle <tmhinkle@gmail.com>
2023-09-27 11:58:26 -0400

    Port over fixes

---
+++ portfolios/Shortcuts.js
@@ -0,0 +1,15 @@
+function addShortcut(sid, root, parents) {
+  let portfolio = getPortfolioFolder(sid);
+  if (!portfolio) {
+    return false
+  }
+  let topLevel = getCommonFolder(PORTFOLIO_HOME);
+  let parentFolder = getCommonFolder(root,topLevel);
+  for (let p of parents) {
+    parentFolder = getNamedChildFolder(parentFolder,p)
+  }
+  let shortcut = DriveApp.createShortcut(portfolio.getId());
+  shortcut.moveTo(parentFolder);
+  return shortcut.getUrl()
+}
+
