bdee4b3b37612a0ea3274109b635573b534bd5ae
Tom Hinkle <tmhinkle@gmail.com>
2023-10-26 10:14:22 -0400

    Prefix constants Pretty

--- portfolio-container-attached/ShortcutGeneratorContainerAttached.js
+++ portfolio-container-attached/ShortcutGeneratorContainerAttached.js
@@ -1,41 +1,46 @@
-function createShortcutMenu (ui) {
-  return ui.createMenu("Shortcuts")
-  .addItem("Set up shortcut sheet","formatShortcutSheet")
-  .addItem("Create shortcuts","createShortcutsFromSheet")
-
+function createShortcutMenu(ui) {
+  return ui
+    .createMenu("Shortcuts")
+    .addItem("Set up shortcut sheet", "formatShortcutSheet")
+    .addItem("Create shortcuts", "createShortcutsFromSheet");
 }
 
-let ROOT_FOLDER = 'Root Folder Name'
-let SUB_FOLDERS = 'Sub-Folder Names (comma separated list)'
+let SC_ROOT_COL = "Root Folder Name";
+let SC_SUB_COL = "Sub-Folder Names (comma separated list)";
+let SC_COMPLETE = "Created";
 // SID defined elsewhere
 
-function getContainerShortcutSheet () {
+function getContainerShortcutSheet() {
   let ss = SpreadsheetApp.getActiveSpreadsheet();
-  let dataSheet = DataSheet(ss,"Shortcuts",[SID,ROOT_FOLDER,SUB_FOLDERS,COMPLETE]);
-  return dataSheet
+  let dataSheet = DataSheet(ss, "Shortcuts", [
+    SID,
+    SC_ROOT_COL,
+    SC_SUB_COL,
+    SC_COMPLETE,
+  ]);
+  return dataSheet;
 }
 
-function formatShortcutSheet () {
+function formatShortcutSheet() {
   let dataSheet = getContainerShortcutSheet();
   let sheet = dataSheet.sheet;
   sheet.setFrozenRows(1);
 }
 
-function createShortcutsFromSheet () {
+function createShortcutsFromSheet() {
   let shortcutDataSheet = getContainerShortcutSheet();
-  for (let rn=1; rn<shortcutDataSheet.length; rn++) {
+  for (let rn = 1; rn < shortcutDataSheet.length; rn++) {
     let row = shortcutDataSheet[rn];
-    if (!row[COMPLETE]) {
-      console.log('Add template for row:',row[SID],row[ROOT_FOLDER],row[PARENTS]);
-      let parents = row[SUB_FOLDERS].split(/\s*,\s*/);
-      console.log('Parnets:',parents);
-      row[COMPLETE] = addShortcut(row[SID],row[ROOT_FOLDER],parents);
-      /*let success = addTemplate(row[SID],row[ROOT_FOLDER],parents);
-      if (success) {
-        row[COMPLETE] = success;
-      } else {
-        row[COMPLETE] = false;
-      }*/
+    if (!row[SC_COMPLETE]) {
+      console.log(
+        "Add template for row:",
+        row[SID],
+        row[SC_ROOT_COL],
+        row[SC_SUB_COL]
+      );
+      let parents = row[SC_SUB_COL].split(/\s*,\s*/);
+      console.log("Parnets:", parents);
+      row[SC_COMPLETE] = addShortcut(row[SID], row[SC_ROOT_COL], parents);
     }
   }
 }
