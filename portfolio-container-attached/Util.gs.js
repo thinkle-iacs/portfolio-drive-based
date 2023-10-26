d2acd52b0eea8fb4faff921775387a1fef171ae5
Tom Hinkle <tmhinkle@gmail.com>
2023-10-26 10:12:09 -0400

    pretty

--- portfolio-container-attached/Util.gs.js
+++ portfolio-container-attached/Util.gs.js
@@ -1,4 +1,5 @@
-/* from: https://stackoverflow.com/questions/16840038/easiest-way-to-get-file-id-from-url-on-google-apps-script 
-*/
-function getIdFromUrl(url) { return url.match(/[-\w]{25,}/); }
-
+/* from: https://stackoverflow.com/questions/16840038/easiest-way-to-get-file-id-from-url-on-google-apps-script
+ */
+function getIdFromUrl(url) {
+  return url.match(/[-\w]{25,}/);
+}
