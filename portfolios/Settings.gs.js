a79a65350f7b79a818261f1a67eaa010a9656392
Tom Hinkle <tmhinkle@gmail.com>
2023-11-02 08:55:21 -0400

    Hardcode all the things... ;)

--- portfolios/Settings.gs.js
+++ portfolios/Settings.gs.js
@@ -3,5 +3,7 @@
 /* Necessary for container-attached scripts
 */
 
-PORTFOLIO_SHEET_HARDCODED = "";
-PORTFOLIO_HOME_HARDCODED = "";
+const PORTFOLIO_SHEET_HARDCODED =
+  "https://docs.google.com/spreadsheets/d/1DIAkXZetN_mbjVFYczvJEFlv6ugnQFGLyTtelUJLMoE/edit";
+const PORTFOLIO_HOME_HARDCODED =
+  "https://drive.google.com/drive/folders/1VZE9D5iN_M4wwp-sv6kjbdxTWwLJpCzj";
