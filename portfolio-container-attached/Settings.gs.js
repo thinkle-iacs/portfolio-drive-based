b15f189eb246a1b9b8a4f3acecf05f278cf6a133
Tom Hinkle <tmhinkle@gmail.com>
2023-11-02 09:24:23 -0400

    ++const

--- portfolio-container-attached/Settings.gs.js
+++ portfolio-container-attached/Settings.gs.js
@@ -3,7 +3,7 @@
 /* Necessary for container-attached scripts
 */
 
-PORTFOLIO_SHEET_HARDCODED =
+const PORTFOLIO_SHEET_HARDCODED =
   "https://docs.google.com/spreadsheets/d/1DIAkXZetN_mbjVFYczvJEFlv6ugnQFGLyTtelUJLMoE/edit";
-PORTFOLIO_HOME_HARDCODED =
+const PORTFOLIO_HOME_HARDCODED =
   "https://drive.google.com/drive/folders/1VZE9D5iN_M4wwp-sv6kjbdxTWwLJpCzj";
